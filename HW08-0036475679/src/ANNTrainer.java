5a8488e183b475e0ad5c84129a7699c983f7b58c
Vinko Kolobara <Vinko.Kolobara@fer.hr>
2015-12-13 20:37:04 +0100

    Gtoovo

--- HW08-0036475679/src/ANNTrainer.java
+++ HW08-0036475679/src/ANNTrainer.java
@@ -10,30 +10,89 @@ import java.util.List;
 public class ANNTrainer {
 
 	public static void main(String[] args) throws IOException {
-		IReadOnlyDataset dataset = loadData("A.dat", 8, 600);
-		TDNN tdnn = new TDNN(new int[] {8,  10,  1}, 
-							 new ITransferFunction[] { new SigmoidTransferFunction(), new SigmoidTransferFunction() }
-							 , dataset);
-		int pop_size = 20;
-		int sol_size = tdnn.getWeightsCount();
-		int max_iter = 200;
-		double merr = 0.01;
-		double f = 0.2;
-		double cr = 0.75;
-		StrategyDEBin strategy = new StrategyDETargetToBestBin(2);
-		IEvaluator evaluator = new EvaluatorTDNN(tdnn);
 		
-		System.out.println(dataset.numberOfSamples() + " " + tdnn.getWeightsCount());
-		
-		AlgorithmDE alg = new AlgorithmDE(pop_size, sol_size, max_iter, merr, f, cr, strategy, evaluator);
-		DoubleArraySolution sol = alg.run();
-		System.out.println(sol);
-		
-		for (int i=0; i<dataset.numberOfSamples(); i++) {
-			double[] outputs = new double[1];
-			tdnn.calcOutputs(dataset.getInputs().get(i), sol.solution, outputs);
-			System.out.println("Očekivano: " + dataset.getOutputs().get(i)[0] + "; Dobiveno: " + outputs[0]);
+		if (args.length != 5) {
+			greska("Nema dovoljan broj argumenata");
 		}
+		
+		try {
+			String path = args[0];
+			String net = args[1];
+			int pop_size = Integer.parseInt(args[2]);
+			double merr = Double.parseDouble(args[3]);
+			int max_iter = Integer.parseInt(args[4]);
+			
+			double f = 0.2;
+			double cr = 0.75;
+			StrategyDEBin strategy = new StrategyDETargetToBestBin(2);
+
+			TDNN tdnn = createNN(net, path);
+			
+			int sol_size = tdnn.getWeightsCount();
+
+			IEvaluator evaluator;
+			
+			if (tdnn instanceof ElmanNN) {
+				evaluator = new EvaluatorElman((ElmanNN) tdnn);
+			} else {
+				evaluator = new EvaluatorTDNN(tdnn);
+			}
+						
+			AlgorithmDE alg = new AlgorithmDE(pop_size, sol_size, max_iter, merr, f, cr, strategy, evaluator);
+			DoubleArraySolution sol = alg.run();
+			
+			IReadOnlyDataset dataset = tdnn.getDataset();
+			
+			for (int i=0; i<dataset.numberOfSamples(); i++) {
+				double[] outputs = new double[1];
+				tdnn.calcOutputs(dataset.getInputs().get(i), sol.solution, outputs);
+				System.out.println("Očekivano: " + dataset.getOutputs().get(i)[0] + "; Dobiveno: " + outputs[0]);
+			}
+			
+			System.out.println(sol + "\n" + "Greška: " + sol.fitness);
+
+		} catch (Exception e) {
+			greska(e.getMessage());
+		}
+		
+	}
+
+
+	private static TDNN createNN(String net, String path) throws IOException {
+		String[] split = net.split("-");
+		String[] arh = split[1].split("x");
+		int[] layers = new int[arh.length];
+		ITransferFunction[] layerFunctions = new ITransferFunction[arh.length-1];
+
+		
+		for (int i=0; i<arh.length; i++) {
+			layers[i] = Integer.parseInt(arh[i]);
+			layerFunctions[(i+1)%layerFunctions.length] = new SigmoidTransferFunction();
+		}
+
+		if (split[0].equals("tdnn")) {
+			IReadOnlyDataset dataset = loadData(path, layers[0], 600);
+			if (layers[layers.length-1] != 1) {
+				greska("Mora biti samo 1 vrijednost na izlazu!");
+			}
+			return new TDNN(layers, layerFunctions, dataset);
+		} else if (split[0].equals("elman")) {
+			IReadOnlyDataset dataset = loadData(path, 600);
+			if (layers[0] != 1 || layers[layers.length-1] != 1) {
+				greska("Mora biti samo 1 ulaz i izlaz!");
+			}
+			return new ElmanNN(layers, layerFunctions, dataset);
+		} else {
+			greska("Nepodržana vrsta mreže!");
+		}
+		
+		return null;
+	}
+
+
+	private static void greska(String string) {
+		System.err.println(string);
+		System.exit(0);
 	}
 
 
@@ -88,7 +147,6 @@ public class ANNTrainer {
 		for (int i=0; i<normalized.length; i++) {
 			normalized[i] = 2.0*(normalized[i] - min) / (max - min) - 1;
 		}
-		System.out.println(Arrays.toString(normalized));
 		return normalized;
 	}
 
